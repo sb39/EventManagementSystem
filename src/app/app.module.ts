4dd69634b0ca60a59e8de76c79f35cd7a551fd1c
#Srij_b <biswassrijeet99@gmail.com>
2018-03-18 12:37:34 +0530

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,54 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from "@angular/router";
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+import { FormioAppConfig } from 'angular-formio';
+import { FormioAuthService, FormioAuthConfig } from 'angular-formio/auth';
+import { AuthConfig, AppConfig } from '../config';
+import { AuthModule } from './auth/auth.module';
+import { HeaderComponent } from './header/header.component';
+
+import { FormioResources } from 'angular-formio/resource';
+
+import { EventModule } from './event/event.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+      },
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      {
+        path: 'auth',
+        loadChildren: () => AuthModule
+      },
+      {
+        path: 'event',
+        loadChildren: () => EventModule
+      }
+    ])
+  ],
+  providers: [
+    FormioAuthService,
+    FormioResources,
+    {provide: FormioAuthConfig, useValue: AuthConfig},
+    {provide: FormioAppConfig, useValue: AppConfig}
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
